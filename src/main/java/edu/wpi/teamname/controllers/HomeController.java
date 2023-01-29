2f4e7c9b5c51389de246b0d09659aad23599f136
Gabrielle Acquista <geacquista@wpi.edu>
2023-01-29 15:12:04 -0500

    play with css for screensize

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -4,13 +4,17 @@ import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class HomeController {
 
   @FXML MFXButton navigateButton;
 
+  @FXML AnchorPane mainPane;
+
   @FXML
   public void initialize() {
+
     navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
   }
 }
